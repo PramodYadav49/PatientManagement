a2a606f92a77384291114b57a4042c0b7748fd09
pramod yadav <indrashyadav8@gmail.com>
2025-12-08 16:13:38 +0530

    Update Doctor Entity

--- src/main/java/com/pm/patientservice/model/Doctor.java
+++ src/main/java/com/pm/patientservice/model/Doctor.java
@@ -1,4 +1,48 @@
 package com.pm.patientservice.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Doctor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String doctorId;
+    private String licenceId;
+    private String specialization;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDoctorId() {
+        return doctorId;
+    }
+
+    public void setDoctorId(String doctorId) {
+        this.doctorId = doctorId;
+    }
+
+    public String getLicenceId() {
+        return licenceId;
+    }
+
+    public void setLicenceId(String licenceId) {
+        this.licenceId = licenceId;
+    }
+
+    public String getSpecialization() {
+        return specialization;
+    }
+
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
+    }
 }
