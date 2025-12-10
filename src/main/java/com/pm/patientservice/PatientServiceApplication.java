e75005f2e9d644052c10d5d1062ac27233c2e166
pramod yadav <indrashyadav8@gmail.com>
2025-12-10 09:57:41 +0530

    adding ApplicationRunner

--- src/main/java/com/pm/patientservice/PatientServiceApplication.java
+++ src/main/java/com/pm/patientservice/PatientServiceApplication.java
@@ -1,14 +1,21 @@
 package com.pm.patientservice;
 
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PatientServiceApplication {
+public class PatientServiceApplication implements ApplicationRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PatientServiceApplication.class, args);
 	}
-//Change commit on main
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        System.out.println("Patient Service I up Now");
+    }
+
 
 }
